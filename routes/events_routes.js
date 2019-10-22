5d916a751bc032d7b5580c4059c6f9f751c417c7
sizsan <sizsan2001@gmail.com>
2019-10-22 10:08:37 +1000

    routes-organised

--- routes/events_routes.js
+++ routes/events_routes.js
@@ -3,13 +3,27 @@ import * as Controller from '../controllers/events_controller';
 const express = require('express');
 const router = express.Router();
 
+// Admin Routes
+
 // Create Event
 router.post('/', Controller.makeNewEvent);
-// Gets all Events
-router.get('/', Controller.getAllEvents);
 // Deletes Event by :id
-// router.delete('/:id', removeProduct);
+// router.delete('/:id', removeEvent);
 // Updates a Event by :id
 // router.put('/:id',changeProduct);
+
+// User Routes
+// Gets all Events
+router.get('/', Controller.getAllEvents);
+// Gets Event by id
+// router.get('/:id', Controller.getEventById);
+// Gets all Events from a particular user
+// router.get('/:user', Controller.getAllEventsByUser);
+
+
+
+
+
+
 
 module.exports = router;
